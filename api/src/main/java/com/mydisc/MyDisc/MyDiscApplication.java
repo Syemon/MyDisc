6f880f243242bbdead4232df65f68166a7eec656
szymon linowski <sz.linowski@gmail.com>
2019-01-22 19:59:01 +0100

    Initialize project

---
+++ api/src/main/java/com/mydisc/MyDisc/MyDiscApplication.java
@@ -0,0 +1,14 @@
+package com.mydisc.MyDisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiscApplication.class, args);
+	}
+
+}
+
---
+++ api/src/test/java/com/mydisc/MyDisc/MyDiscApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mydisc.MyDisc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyDiscApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
