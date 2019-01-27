c51ac4164bf6a588ae2b649babb630ec4da25166
szymon linowski <sz.linowski@gmail.com>
2019-01-27 19:46:33 +0100

    Add get folder tests

--- api/src/test/java/com/mydisc/MyDisc/controller/integration/FolderControllerTests.java
+++ api/src/test/java/com/mydisc/MyDisc/controller/integration/FolderControllerTests.java
@@ -4,6 +4,7 @@ import com.mydisc.MyDisc.MyDiscApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MyDiscApplication.class)
@@ -46,5 +49,37 @@ public class FolderControllerTests {
                 .andExpect(
                         content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void testGetWithNoCreatedFolders() throws Exception {
+        this.mockMvc.perform(get("/api/folders"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "Lorem", 0 },
+                { 123, 1 },
+                { "b40f2063-2921-4599-abdd-6469199bc548", 2 }
+        });
+    }
+
+    @Parameterized.Parameter
+    public int input;
+
+    @Parameterized.Parameter(1)
+    public int expected;
+
+    @Test
+    public void testGetValidation() throws Exception {
+        this.mockMvc.perform(get("/api/folders/{serviceId}", input))
+                .andExpect(
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
