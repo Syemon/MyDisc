ca76a21423282ef4f2b2faced47ae29dc8652aba
szymon linowski <sz.linowski@gmail.com>
2019-01-30 20:24:33 +0100

    Add folder parent-children self join

--- api/src/main/java/com/mydisc/MyDisc/entity/Folder.java
+++ api/src/main/java/com/mydisc/MyDisc/entity/Folder.java
@@ -4,6 +4,8 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -19,6 +21,13 @@ public class Folder {
     @NotNull
     @Column(name="name")
     private String name;
+
+    @ManyToOne(cascade={CascadeType.ALL})
+    @JoinColumn(name="parent_id")
+    private Folder parent;
+
+    @OneToMany(mappedBy="parent")
+    private Set<Folder> children = new HashSet<Folder>();
 
     public Folder() {
 
@@ -40,11 +49,38 @@ public class Folder {
         this.name = name;
     }
 
+    public Folder getParent() {
+        return parent;
+    }
+
+    public void setParent(Folder parent) {
+        this.parent = parent;
+        parent.addChild(this);
+    }
+
+    public Set<Folder> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Set<Folder> children) {
+        this.children = children;
+    }
+
+    private void addChild(Folder child) {
+        this.getChildren().add(child);
+    }
+
+    public void removeChild(Folder child) {
+        this.getChildren().remove(child);
+    }
+
     @Override
     public String toString() {
         return "Folder{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", parent=" + parent +
+                ", children=" + children +
                 '}';
     }
 }
---
+++ api/src/test/java/com/mydisc/MyDisc/entity/unit/FolderTests.java
@@ -0,0 +1,60 @@
+package com.mydisc.MyDisc.entity.unit;
+
+import com.mydisc.MyDisc.entity.Folder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class FolderTests {
+
+    private Folder folder;
+
+    @Before
+    public void setUp() {
+        this.folder  = new Folder();
+    }
+
+    @Test
+    public void testName() {
+        String name = "Lorem";
+        this.folder.setName(name);
+        Assert.assertEquals(name, this.folder.getName());
+    }
+
+    @Test
+    public void testSetParent() {
+        Folder parent = new Folder("parent");
+        Folder children1 = new Folder("children1");
+        Folder children2 = new Folder("children2");
+
+        children1.setParent(parent);
+        children2.setParent(parent);
+
+        Assert.assertEquals(parent, children1.getParent());
+        Assert.assertEquals(parent, children2.getParent());
+
+        int numberOfChildren = parent.getChildren().size();
+        Assert.assertEquals(2, numberOfChildren);
+    }
+
+    @Test
+    public void testRemoveChild() {
+        Folder parent = new Folder("parent");
+        Folder children1 = new Folder("children1");
+        Folder children2 = new Folder("children2");
+
+        children1.setParent(parent);
+        children2.setParent(parent);
+
+        parent.removeChild(children1);
+        parent.removeChild(children2);
+
+        int numberOfChildren = parent.getChildren().size();
+        Assert.assertEquals(0, numberOfChildren);
+    }
+}
