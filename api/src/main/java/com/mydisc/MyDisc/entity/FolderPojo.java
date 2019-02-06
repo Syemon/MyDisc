1bb44cbe9061b347400a5e9c9cff4b57a46ca7be
szymon linowski <sz.linowski@gmail.com>
2019-02-06 20:52:58 +0100

    Add folder POJO

---
+++ api/src/main/java/com/mydisc/MyDisc/entity/FolderPojo.java
@@ -0,0 +1,35 @@
+package com.mydisc.MyDisc.entity;
+
+public class FolderPojo {
+
+    private String id;
+    private String parentId;
+    private String name;
+
+    public FolderPojo() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
