8da68195b6aa3f3cf08c90fccb9fdd368f08c5a8
szymon linowski <sz.linowski@gmail.com>
2019-02-06 20:51:59 +0100

    Update folder entity tests

--- api/src/test/java/com/mydisc/MyDisc/entity/unit/FolderTests.java
+++ api/src/test/java/com/mydisc/MyDisc/entity/unit/FolderTests.java
@@ -5,14 +5,21 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class FolderTests {
 
     private Folder folder;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Before
     public void setUp() {
@@ -27,6 +34,7 @@ public class FolderTests {
     }
 
     @Test
+    @Transactional
     public void testSetParent() {
         Folder parent = new Folder("parent");
         Folder children1 = new Folder("children1");
