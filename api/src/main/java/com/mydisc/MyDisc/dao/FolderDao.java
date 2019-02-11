82989d56a0c60bd2711b4ba5c7cadbcd0bbd8e16
szymon linowski <sz.linowski@gmail.com>
2019-02-11 20:41:35 +0100

    Delete unused imports

--- api/src/main/java/com/mydisc/MyDisc/dao/FolderDao.java
+++ api/src/main/java/com/mydisc/MyDisc/dao/FolderDao.java
@@ -3,10 +3,8 @@ package com.mydisc.MyDisc.dao;
 
 import com.mydisc.MyDisc.entity.Folder;
 import com.mydisc.MyDisc.entity.FolderPojo;
-import org.springframework.http.HttpEntity;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public interface FolderDao {
