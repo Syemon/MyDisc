2145012fdbf4c3911cb7e82533784e653885290a
szymon linowski <sz.linowski@gmail.com>
2019-02-28 12:51:29 +0100

    Add early file entity

---
+++ api/src/main/java/com/mydisc/MyDisc/entity/File.java
@@ -0,0 +1,83 @@
+package com.mydisc.MyDisc.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+@Table(name="file")
+public class File {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+    private UUID id;
+
+    @ManyToOne(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="folder_id")
+    private Folder folder;
+
+    @NotNull
+    @Column(name="name")
+    private String name;
+
+    @NotNull
+    @Column(name="type")
+    private String type;
+
+    @NotNull
+    @Column(name="size")
+    private Long size;
+
+    @Nullable
+    @Column(name="deleted")
+    private boolean deleted;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
