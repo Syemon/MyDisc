4290b9e0b15f79b216bb71beac4e75debbae00c5
szymon linowski <sz.linowski@gmail.com>
2019-08-24 12:19:33 +0200

    Move annotation from method to class

--- api/src/main/java/com/mydisc/MyDisc/service/FileService.java
+++ api/src/main/java/com/mydisc/MyDisc/service/FileService.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.*;
 
+@Transactional
 @Service
 public class FileService {
     private FileDao fileDao;
@@ -28,7 +29,6 @@ public class FileService {
         this.fileStorageService = fileStorageService;
     }
 
-    @Transactional
     public ResponseEntity<Resource> download(UUID fileId) {
         File file = findById(fileId);
 
@@ -41,17 +41,14 @@ public class FileService {
                 .body(fileResource);
     }
 
-    @Transactional
     public File findById(UUID fileId) {
         return fileDao.findById(fileId);
     }
 
-    @Transactional
     public File findById(UUID folderId, UUID fileId) {
         return fileDao.findById(folderId, fileId);
     }
 
-    @Transactional
     public void delete(UUID fileId) {
         String storageName = fileDao.findById(fileId).getStorageName();
         fileDao.delete(fileId);
@@ -65,27 +62,22 @@ public class FileService {
         }
     }
 
-    @Transactional
     public boolean exists(UUID fileId) {
         return fileDao.exists(fileId);
     }
 
-    @Transactional
     public boolean exists(UUID folderId, UUID fileId) {
         return fileDao.exists(folderId, fileId);
     }
 
-    @Transactional
     public List<File> list() {
         return fileDao.list();
     }
 
-    @Transactional
     public List<File> list(UUID folderId) {
         return fileDao.list(folderId);
     }
 
-    @Transactional
     public Resources<FileResource> getFileResources(List<File> files) {
         List<FileResource> fileResources = new ArrayList<>();
 
@@ -101,31 +93,26 @@ public class FileService {
         return new Resources<>(fileResources);
     }
 
-    @Transactional
     public File upload(MultipartFile rawFile) {
         Map<String, String> fileNames = fileStorageService.storeFile(rawFile);
 
         return fileDao.save(rawFile, fileNames);
     }
 
-    @Transactional
     public File upload(UUID folderId, MultipartFile rawFile) {
         Map<String, String> fileNames = fileStorageService.storeFile(rawFile);
 
         return fileDao.save(folderId, rawFile, fileNames);
     }
 
-    @Transactional
     public void move(UUID fileId, FilePojo filePojo) {
         fileDao.move(fileId, filePojo);
     }
 
-    @Transactional
     public void move(UUID folderId, UUID fileId, FilePojo filePojo) {
         fileDao.move(folderId, fileId, filePojo);
     }
 
-    @Transactional
     public boolean isEnoughSpace(MultipartFile file) {
         return fileDao.isEnoughSpace(file);
     }
--- api/src/main/java/com/mydisc/MyDisc/service/FolderService.java
+++ api/src/main/java/com/mydisc/MyDisc/service/FolderService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
+@Transactional
 @Service
 public class FolderService {
     private FolderDao folderDao;
@@ -19,47 +20,38 @@ public class FolderService {
         this.folderDao = folderDao;
     }
 
-    @Transactional
     public boolean exists(UUID folderId) {
         return folderDao.exists(folderId);
     }
 
-    @Transactional
     public Folder findById(UUID id) {
         return folderDao.findById(id);
     }
 
-    @Transactional
     public List<Folder> findChildren() {
         return folderDao.findChildren();
     }
 
-    @Transactional
     public List<Folder> findChildren(UUID folderId) {
         return folderDao.findChildren(folderId);
     }
 
-    @Transactional
     public Folder save(FolderPojo folderPojo) {
         return folderDao.save(folderPojo);
     }
 
-    @Transactional
     public Folder rename(FolderPojo folderPojo) {
         return folderDao.update(folderPojo);
     }
 
-    @Transactional
     public void delete(UUID id) {
         folderDao.deleteById(id);
     }
 
-    @Transactional
     public void move(UUID folderId) {
         folderDao.move(folderId);
     }
 
-    @Transactional
     public void move(UUID folderId, UUID targetFolderId) {
         folderDao.move(folderId, targetFolderId);
     }
