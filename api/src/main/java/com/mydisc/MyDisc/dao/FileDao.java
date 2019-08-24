e233176fdb89b809527eda1b763cd8c8fbe69daf
szymon linowski <sz.linowski@gmail.com>
2019-08-24 12:43:12 +0200

    Refactor

--- api/src/main/java/com/mydisc/MyDisc/dao/FileDao.java
+++ api/src/main/java/com/mydisc/MyDisc/dao/FileDao.java
@@ -2,34 +2,188 @@ package com.mydisc.MyDisc.dao;
 
 import com.mydisc.MyDisc.entity.File;
 import com.mydisc.MyDisc.entity.FilePojo;
+import com.mydisc.MyDisc.entity.Folder;
+import com.mydisc.MyDisc.property.FileStorageProperties;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public interface FileDao {
-    File findById(UUID fileId);
+@Repository
+public class FileDao {
+    private EntityManager entityManager;
+    private FileStorageProperties fileStorageProperties;
+    private FolderDao folderDao;
 
-    File findById(UUID folderId, UUID fileId);
+    @Autowired
+    public FileDao(
+            EntityManager entityManager,
+            FileStorageProperties fileStorageProperties,
+            FolderDao folderDao
+    ) {
+        this.entityManager = entityManager;
+        this.fileStorageProperties = fileStorageProperties;
+        this.folderDao = folderDao;
+    }
 
-    void delete(UUID fileId);
+    public File findById(UUID fileId) {
+        Session session = entityManager.unwrap(Session.class);
 
-    List<File> list();
+        return session.get(File.class, fileId);
+    }
 
-    List<File> list(UUID folderId);
+    public File findById(UUID folderId, UUID fileId) {
+        Session session = entityManager.unwrap(Session.class);
 
-    File save(MultipartFile rawFile, Map<String, String> fileNames);
+        Query<File> query =
+                session.createQuery(
+                        "SELECT fi FROM File AS fi \n" +
+                                "INNER JOIN fi.folder AS fo \n" +
+                                "WHERE fo.id = :folderId AND fi.id = :fileId", File.class);
 
-    File save(UUID folderId, MultipartFile rawFile, Map<String, String> fileNames);
+        query.setParameter("folderId", folderId);
+        query.setParameter("fileId", fileId);
 
-    void move(UUID fileId, FilePojo filePojo);
+        return query.getSingleResult();
+    }
 
-    void move(UUID folderId, UUID fileId, FilePojo filePojo);
+    public void delete(UUID fileId) {
+        Session session = entityManager.unwrap(Session.class);
 
-    boolean exists(UUID fileId);
+        Query query = session.createQuery(
+                "delete from File where id = :fileId");
 
-    boolean exists(UUID folderId, UUID fileId);
+        query.setParameter("fileId", fileId)
+                .executeUpdate();
+    }
 
-    boolean isEnoughSpace(MultipartFile file);
+    public boolean exists(UUID fileId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query query = session.createQuery(
+                "SELECT 1 " +
+                        "FROM File " +
+                        "WHERE id = :fileId");
+
+        return query.setParameter("fileId", fileId)
+                .uniqueResult() != null;
+    }
+
+    public boolean exists(UUID folderId, UUID fileId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query query = session.createQuery(
+                "SELECT 1 " +
+                        "FROM File " +
+                        "WHERE id = :fileId AND folder_id = :folderId");
+
+        return query.setParameter("fileId", fileId)
+                .setParameter("folderId", folderId)
+                .uniqueResult() != null;
+    }
+
+    public List<File> list() {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<File> query =
+                session.createQuery(
+                        "SELECT f FROM File AS f \n" +
+                                "WHERE f.folder IS NULL", File.class);
+
+        return query.getResultList();
+    }
+
+    public List<File> list(UUID folderId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<File> query =
+                session.createQuery(
+                        "SELECT fi FROM File AS fi \n" +
+                                "INNER JOIN fi.folder AS fo \n" +
+                                "WHERE fo.id = :folderId", File.class);
+
+        query.setParameter("folderId", folderId);
+
+        return query.getResultList();
+    }
+
+    public File save(MultipartFile rawFile, Map<String, String> fileNames) {
+        Session session = entityManager.unwrap(Session.class);
+
+        File file = new File();
+        file.setName(fileNames.get("fileName"));
+        file.setStorageName(fileNames.get("storageFileName"));
+        file.setType(rawFile.getContentType());
+        file.setSize(rawFile.getSize());
+
+        session.save(file);
+        session.flush();
+
+        return file;
+    }
+
+    public File save(UUID folderId, MultipartFile rawFile, Map<String, String> fileNames) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = folderDao.findById(folderId);
+        File file = new File();
+        file.setFolder(folder);
+        file.setName(fileNames.get("fileName"));
+        file.setStorageName(fileNames.get("storageFileName"));
+        file.setType(rawFile.getContentType());
+        file.setSize(rawFile.getSize());
+
+        session.save(file);
+        session.flush();
+
+        return file;
+    }
+
+    public void move(UUID fileId, FilePojo filePojo) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = session.get(Folder.class, filePojo.getFolderId());
+        File file = session.get(File.class, fileId);
+        file.setFolder(folder);
+        session.save(file);
+    }
+
+    public void move(UUID folderId, UUID fileId, FilePojo filePojo) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<File> query =
+                session.createQuery(
+                        "SELECT f FROM File AS f \n" +
+                                "WHERE f.folder.id = :folderId \n" +
+                                "AND f.id = :fileId", File.class);
+
+        query.setParameter("folderId", folderId);
+        query.setParameter("fileId", fileId);
+
+        File file = query.getSingleResult();
+        Folder folder = session.get(Folder.class, filePojo.getFolderId());
+
+        file.setFolder(folder);
+        session.save(file);
+    }
+
+    public boolean isEnoughSpace(MultipartFile file) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Query query = session.createQuery(
+                "SELECT COALESCE(SUM(size), 0) + :size " +
+                        "FROM File");
+
+        Object size = query.
+                setParameter("size", file.getSize()).
+                getSingleResult();
+
+        return Long.parseLong(String.valueOf(size)) < fileStorageProperties.getStorageSpace();
+    }
 }
--- api/src/main/java/com/mydisc/MyDisc/dao/FolderDao.java
+++ api/src/main/java/com/mydisc/MyDisc/dao/FolderDao.java
@@ -1,30 +1,128 @@
 package com.mydisc.MyDisc.dao;
 
-
 import com.mydisc.MyDisc.entity.Folder;
 import com.mydisc.MyDisc.entity.FolderPojo;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
+import javax.persistence.EntityManager;
+import java.util.*;
 
-public interface FolderDao {
-    boolean exists(UUID folderId);
+@Repository
+public class FolderDao {
+    private EntityManager entityManager;
 
-    List<Folder> findAll();
+    @Autowired
+    public FolderDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
-    Folder findById(UUID id);
+    public boolean exists(UUID folderId) {
+        Session session = entityManager.unwrap(Session.class);
 
-    Folder save(FolderPojo folderPojo);
+        Query query = session.createQuery(
+                "SELECT 1 " +
+                        "FROM Folder " +
+                        "WHERE id=:folderId");
 
-    Folder update(FolderPojo folderPojo);
+        return query.setParameter("folderId", folderId)
+                .uniqueResult() != null;
+    }
 
-    void deleteById(UUID id);
+    public Folder findById(UUID id) {
+        Session session = entityManager.unwrap(Session.class);
 
-    List<Folder> findChildren();
+        return session.get(Folder.class, id);
+    }
 
-    List<Folder> findChildren(UUID folderId);
+    public List<Folder> findChildren() {
+        Session session = entityManager.unwrap(Session.class);
 
-    void move(UUID folderId);
+        Query<Folder> query =
+                session.createQuery(
+                        "SELECT f FROM Folder AS f \n" +
+                        "LEFT JOIN Folder AS p \n" +
+                        "ON f.parent = p \n" +
+                        "WHERE p IS NULL", Folder.class);
 
-    void move(UUID folderId, UUID targetFolderId);
+        return query.getResultList();
+    }
+
+    public List<Folder> findChildren(UUID folderId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = session.get(Folder.class, folderId);
+
+        return folder.getChildren();
+    }
+
+    public Folder save(FolderPojo folderPojo) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = new Folder();
+        folder.setName(folderPojo.getName());
+        session.save(folder);
+        session.flush();
+        session.refresh(folder);
+
+        try {
+            UUID parentId = UUID.fromString(folderPojo.getParentId());
+            Folder parent = session.get(Folder.class, parentId);
+            folder.setParent(parent);
+        } catch (Exception exc) {
+
+        }
+
+        session.save(folder);
+        return folder;
+    }
+
+    public Folder update(FolderPojo folderPojo) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = session.get(Folder.class, folderPojo.getId());
+        folder.setName(folderPojo.getName());
+        session.save(folder);
+
+        return folder;
+    }
+
+    public void deleteById(UUID id) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = findById(id);
+
+        session.remove(folder);
+    }
+
+    public void move(UUID folderId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = session.get(Folder.class, folderId);
+
+        folder.setParent(null);
+
+        session.save(folder);
+    }
+
+    public void move(UUID folderId, UUID targetFolderId) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Folder folder = session.get(Folder.class, folderId);
+        Folder targetFolder = session.get(Folder.class, targetFolderId);
+
+        if (null != folder.getParent()) {
+            Folder parent = folder.getParent();
+            parent.removeChild(folder);
+            session.save(parent);
+        }
+
+        folder.setParent(targetFolder);
+        targetFolder.removeChild(folder);
+
+        session.save(folder);
+        session.save(targetFolder);
+    }
 }
